4d3890067fa133cb560fb16dfe13d0166121ff32
Sean Mills <scmills0495@gmail.com>
2015-02-25 15:28:06 -0800

    change js file

--- raceCars.js
+++ raceCars.js
@@ -54,37 +54,3 @@ $(document).ready(function() {
   //  (this).find("div").slideToggle();
   //})
 });
-
-
-//JQUERY
-//$(document).ready(function() {
-//  $("#tour").on("click", "button", function() {
-//    $(".photos").slideToggle();
-//  });
-//  $(".photos").on("mouseenter", "li", function() {
-//    $(this).find("span").slideToggle();
-//  }).on("mouseleave", "li", function() {
-//    $(this).find("span").slideToggle();
-//  });
-//});
-//HTML OUTLINE
-//<div id="tour">
-//  <h2>Paris, France Tour</h2>
-//  <p>$2,499 for 7 Nights</p>
-//  <button>See photos from our last tour</button>
-//  <ul class="photos">
-//    <li>
-//      <img src="/assets/photos/paris1.jpg">
-//      <span>Arc de Triomphe</span>
-//    </li>
-//    <li>
-//      <img src="/assets/photos/paris2.jpg">
-//      <span>The Eiffel Tower</span>
-//    </li>
-//    <li>
-//      <img src="/assets/photos/paris3.jpg">
-//      <span>Notre Dame de Paris</span>
-//    </li>
-//  </ul>
-//</div>
-
